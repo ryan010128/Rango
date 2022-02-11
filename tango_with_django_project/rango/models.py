4662640b59cf472e4e200427c6cd0394cfd1290a
2515111l@student.gla.ac.uk <2515111l@student.gla.ac.uk>
2022-02-12 05:15:16 +0800

    Chapter9 works

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
+    NAME_MAX_LENGTH = 128
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -17,6 +20,9 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
@@ -24,3 +30,12 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = '/rango/login/'
+
+# Passsword hash
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
