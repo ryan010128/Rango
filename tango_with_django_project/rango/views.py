28b6a60dff86fb7233540b92a77b68c53e6b0cd9
2515111l@student.gla.ac.uk <2515111l@student.gla.ac.uk>
2022-02-12 02:55:00 +0800

    chapter8 continued

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -14,11 +14,11 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context = context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by ryanliu.'}
-    return render(request, 'rango/about.html', context = context_dict)
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
