314969940deebee8e56de17ff921b1a2182d7090
2515111l@student.gla.ac.uk <2515111l@student.gla.ac.uk>
2022-02-13 07:46:28 +0800

    Fixed chapter10 bug for cookies not retrieving from serverside

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -10,8 +10,8 @@ from rango.forms import CategoryForm, PageForm
 from datetime import datetime
 
 def visiter_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
     if(datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
